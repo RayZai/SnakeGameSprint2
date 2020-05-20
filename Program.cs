4b70829144f84d61692b169b56c30396f3a8af12
Error404-FailToLoadName <62086468+Error404-FailToLoadName@users.noreply.github.com>
2020-05-20 15:35:44 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -440,7 +440,7 @@ namespace Snake
 						Console.ForegroundColor = ConsoleColor.Yellow;
 						if (superFood == true)
 						{
-							Console.Write("$");
+							Console.Write("$$");
 						}
 						else
 						{

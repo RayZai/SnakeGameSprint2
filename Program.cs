1537151a50a5f3e708ac35fac70a214838a32291
Hans <48939406+WoshiiAlongKia@users.noreply.github.com>
2020-05-26 12:34:46 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +7,8 @@ using System.Threading;
 using System.IO;
 using System.ComponentModel.Design;
 using System.Runtime.CompilerServices;
+using System.Drawing;
+using System.ComponentModel;
 
 
 namespace Snake
